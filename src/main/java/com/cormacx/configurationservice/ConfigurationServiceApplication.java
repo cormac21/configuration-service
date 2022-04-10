9a01857db9a482c0cbd92dbbadd2c4f184771f0d
cormac21 <gaio.lucas0@gmail.com>
2022-04-10 20:26:11 -0300

    Forgot the annotation for clarity

--- src/main/java/com/cormacx/configurationservice/ConfigurationServiceApplication.java
+++ src/main/java/com/cormacx/configurationservice/ConfigurationServiceApplication.java
@@ -7,6 +7,7 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
 @EnableConfigServer
+@EnableDiscoveryClient
 public class ConfigurationServiceApplication {
 
 	public static void main(String[] args) {
